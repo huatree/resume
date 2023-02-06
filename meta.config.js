f48399fcc9064dadf92ff28fa3dead2d3be2b70c
huatree <wang.yaohua@foxmail.com>
2023-02-06 23:03:19 +0800

    🎈 perf(首页): 优化个人简历

--- meta.config.js
+++ meta.config.js
@@ -1,26 +1,12 @@
 export default {
-    name: "醉梦",
-    baseInfo: [
-        {
-            title: "基本信息",
-            subTitle: "96 年 / 男 / 本科 / 上海",
-            content: "2020 年，计算机科学与技术专业入行前端。曾在医疗、电商等行业公司任职前端负责人、开发工程师，现阶段主要工作方向是前端开发、研发流程的搭建与优化。"
-        },
-        {
-            title: "技术能力",
-            content: "熟悉 Vue 技术栈及开发生态\n熟悉混合应用开发对前端工程化、模块化、组件化、规范化，性能优化、前端安全等有一定的应用与思考\n熟练使用 Git 分布式管理工具\n掌握 Linux 服务器基本的运维能力，自建过 CI/CD 持续集成交付环境\n其他掌握的非主力开发能力：NodeJS、React、小程序等"
-        },
-        {
-            title: "前端作品",
-            content: "篇幅有限，不一一列举，我的更多前端项目请点此查看"
-        },
-        {
-            title: "职业经历",
-            content: "2021.07 - 今 电子商务有限公司(>2000 人) H5 前端开发工程师\n2020.06 - 2021.06 健康科技有限公司(>20 人) 前端负责人"
-        },
-        {
-            title: "联系方式",
-            content: "blog: https://www.huatree.top\ngithub: https://github.com/huatree\nemail: wang.yaohua@foxmail.com"
-        }
-    ]
+    beseInfo: {
+        name: "醉梦",
+        job: "前端工程师",
+        year: "3年工作经验",
+        weixin: "hua4F5DA2",
+        phone: "17717892139",
+        email: "wang.yaohua@foxmail.com",
+        city: "Shanghai",
+        github: "GitHub"
+    },
 };
